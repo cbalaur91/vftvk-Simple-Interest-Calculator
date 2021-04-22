6ba54bee8acbc1f40e8fd2586390955ba0bebbb8
Cosmin Balaur <cbalaur91@gmail.com>
2021-04-22 13:53:24 +0000

    final change

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    // variables required for calculating the interest
 
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
@@ -8,10 +9,19 @@ function compute()
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
     
-    var result =  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
-    document.getElementById("result").innerText = result;
+    //var result =  document.getElementById("result").value;
+    document.getElementById("result").innerHTML ="\<br\>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";;
 }
 
+function validate(){
+    var amount = document.getElementById("principal").value;
+    if (amount <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+}
+// function to update the rate slider
 function updateRate(){
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
